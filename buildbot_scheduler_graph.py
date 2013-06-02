1a9edecc0040786b567b229c6f683d5aa5e1dff5
Ben Hearsum <bhearsum@gmail.com>
2013-06-01 22:56:19 -0400

    First stab at node merging, not testing at all yte.

--- buildbot_scheduler_graph.py
+++ buildbot_scheduler_graph.py
@@ -5,12 +5,15 @@ import json
 import logging
 import os
 import os.path
+import re
 import sys
 
-__version__ = '1.0'
+__version__ = "1.0"
 
 logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s")
 log = logging.getLogger(__name__)
+
+chunked_builder_pattern = "(?P<basename>.*) \d+/\d+$"
 
 def parse_schedulers(schedulers, triggerables={}):
     """Parses Scheduler data into a dict whose keys are the name of
@@ -25,53 +28,53 @@ def parse_schedulers(schedulers, triggerables={}):
     """
     graph_info = {}
     for s in schedulers:
-        # Some schedulers have the same name as their builders, so let's be sure
+        # Some schedulers have the same name as their builders, so let"s be sure
         # to avoid conflicts
         scheduler_name = "%s scheduler" % s.name
         log.debug("Creating graph for Scheduler: %s" % scheduler_name)
         graph = graph_info[scheduler_name] = defaultdict(set)
-        graph['nodes'].add(scheduler_name)
-        graph_info[scheduler_name]['root'] = True
+        graph["nodes"].add(scheduler_name)
+        graph_info[scheduler_name]["root"] = True
         for builder in s.builderNames:
             log.debug("  Adding Builder: %s" % builder)
-            graph['nodes'].add(builder)
-            graph['edges'].add((scheduler_name, builder))
+            graph["nodes"].add(builder)
+            graph["edges"].add((scheduler_name, builder))
         if getattr(s, "trigger", None):
-            graph_info[scheduler_name]['root'] = False
+            graph_info[scheduler_name]["root"] = False
             if s.name in triggerables:
                 log.debug("  Hooking up Triggerable Scheduler Builders")
             for builder in triggerables.get(s.name, []):
-                graph['nodes'].add(builder)
-                graph['edges'].add((builder, scheduler_name))
+                graph["nodes"].add(builder)
+                graph["edges"].add((builder, scheduler_name))
         # Connect Dependent Schedulers together
         if getattr(s, "upstream_name", None):
             log.debug("  Adding Dependent Scheduler: %s" % s.upstream_name)
-            graph_info[scheduler_name]['root'] = False
+            graph_info[scheduler_name]["root"] = False
             for upstream in schedulers:
                 if upstream.name == s.upstream_name:
                     for builder in upstream.builderNames:
-                        graph['nodes'].add(builder)
-                        graph['edges'].add((builder, scheduler_name))
+                        graph["nodes"].add(builder)
+                        graph["edges"].add((builder, scheduler_name))
         # Connect AggregatingScheduler Builders together
         if getattr(s, "upstreamBuilders", None):
             log.debug("  Adding Builders from an AggregatingScheduler:")
-            graph_info[scheduler_name]['root'] = False
+            graph_info[scheduler_name]["root"] = False
             for builder in s.upstreamBuilders:
                 log.debug("    %s" % builder)
-                graph['nodes'].add(builder)
-                graph['edges'].add((builder, scheduler_name))
+                graph["nodes"].add(builder)
+                graph["edges"].add((builder, scheduler_name))
 
     return graph_info
 
 
 def merge_graph_info(graph_info):
-    # We're only going to be returning root Schedulers, but we need somewhere
+    # We"re only going to be returning root Schedulers, but we need somewhere
     # to do all of the merging in the meantime, including merging one non-root
     # Scheduler into another.
     new_graph_info = deepcopy(graph_info)
     for s in graph_info:
         # Root Schedulers never need to be merged
-        if graph_info[s]['root'] == True:
+        if graph_info[s]["root"] == True:
             pass
 
         log.debug("Evaluating %s for merging" % s)
@@ -82,16 +85,63 @@ def merge_graph_info(graph_info):
             if s == other_s:
                 continue
             log.debug("  Looking at nodes from %s: %s" % (other_s, new_graph_info[other_s]))
-            # If the upstream side of any edge appears in another Scheduler's
+            # If the upstream side of any edge appears in another Scheduler"s
             # builders, we should merge this one into it.
-            for edge in graph_info[s]['edges']:
-                if edge[0] in new_graph_info[other_s]['nodes']:
+            for edge in graph_info[s]["edges"]:
+                if edge[0] in new_graph_info[other_s]["nodes"]:
                     log.debug("    Found node %s, merging" % edge[0])
-                    new_graph_info[other_s]['nodes'].update(new_graph_info[s]['nodes'])
-                    new_graph_info[other_s]['edges'].update(new_graph_info[s]['edges'])
+                    new_graph_info[other_s]["nodes"].update(new_graph_info[s]["nodes"])
+                    new_graph_info[other_s]["edges"].update(new_graph_info[s]["edges"])
                     break
 
-    return {s:info for s,info in new_graph_info.iteritems() if info['root']}
+    return {s:info for s,info in new_graph_info.iteritems() if info["root"]}
+
+
+def merge_nodes(nodes, edges, merge_pattern=chunked_builder_pattern):
+    merged_nodes = set()
+    merged_edges = set()
+    r = re.compile(merge_pattern)
+    node_groups = defaultdict(list)
+    node_edges = defaultdict(list)
+    for n in nodes:
+        m = r.match(n)
+        if m:
+            basename = m.groupdict()["basename"]
+            node_groups[basename].append(n)
+
+    for e in edges:
+        node_edges[e[0]].append(e)
+        node_edges[e[1]].append(e)
+
+    for basename, nodes in node_groups.iteritems():
+        # Can't merge a group with only one item!
+        if len(nodes) < 2:
+            merged_nodes.update(nodes)
+            continue
+
+        mergeable = True
+
+        log.debug("Trying to merge node group '%s'" % basename)
+        # Nodes can only be merged together if all nodes in the group have
+        # the same edges.
+        required_edges = node_edges[nodes[0]]
+        for n in nodes[1:]:
+            if len(node_edges[n]) != len(required_edges):
+                mergeable = False
+                break
+            # Replace the current node with the first node in all of the edges, for purposes of easy comparison
+            replaced_edges = set([(left.replace(nodes[0], n), right.replace(nodes[0], n)) for left, right in node_edges[n]])
+            if required_edges != replaced_edges:
+                mergeable = False
+                break
+
+        if mergeable:
+            merged_nodes.add(basename)
+            for n in nodes:
+                for e in node_edges[n]:
+                    merged_edges.add((e[0].replace(n, basename), e[1].replace(n, basename)))
+        else:
+            merged_nodes.update(nodes)
 
 
 def main():
@@ -99,10 +149,10 @@ def main():
     import pydot
 
     parser = ArgumentParser()
-    parser.add_argument('master_cfg', nargs=1)
-    parser.add_argument('output_dir', nargs=1)
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False)
-    parser.add_argument('-t', '--triggerables', dest='triggerables')
+    parser.add_argument("master_cfg", nargs=1)
+    parser.add_argument("output_dir", nargs=1)
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False)
+    parser.add_argument("-t", "--triggerables", dest="triggerables")
 
     args = parser.parse_args()
     master_cfg = os.path.abspath(args.master_cfg[0])
@@ -123,14 +173,14 @@ def main():
         # Put the current directory in sys.path, in case there are imported
         # files there.
         sys.path.insert(0, "")
-        cfg = load_source('cfg', master_cfg)
+        cfg = load_source("cfg", master_cfg)
 
-        graph_info = parse_schedulers(cfg.c['schedulers'], triggerables=triggerables)
+        graph_info = parse_schedulers(cfg.c["schedulers"], triggerables=triggerables)
         for name, graph_info in merge_graph_info(graph_info).iteritems():
-            graph = pydot.Dot(graph_type='digraph')
-            for node in graph_info['nodes']:
+            graph = pydot.Dot(graph_type="digraph")
+            for node in graph_info["nodes"]:
                 graph.add_node(pydot.Node(node))
-            for edge in graph_info['edges']:
+            for edge in graph_info["edges"]:
                 graph.add_edge(pydot.Edge(*edge))
             graph.write_png(os.path.join(output_dir, "%s.png" % name))
 
@@ -138,5 +188,5 @@ def main():
         os.chdir(curdir)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- test_buildbot_scheduler_graph.py
+++ test_buildbot_scheduler_graph.py
@@ -1,6 +1,7 @@
 import unittest
 
-from buildbot_scheduler_graph import parse_schedulers, merge_graph_info
+from buildbot_scheduler_graph import parse_schedulers, merge_graph_info, \
+    merge_nodes
 
 class Scheduler(object):
     def __init__(self, name, builderNames):
@@ -23,110 +24,110 @@ class AggregatingScheduler(Scheduler):
 
 class TestCombineObjects(unittest.TestCase):
     def testSimpleScheduler(self):
-        s = [Scheduler('foo', ('bar', 'baz'))]
+        s = [Scheduler("foo", ("bar", "baz"))]
         expected = {
-            'foo scheduler': {
-                'nodes': set(('foo scheduler', 'bar', 'baz')),
-                'edges': set((('foo scheduler', 'bar'), ('foo scheduler', 'baz'))),
-                'root': True,
+            "foo scheduler": {
+                "nodes": set(("foo scheduler", "bar", "baz")),
+                "edges": set((("foo scheduler", "bar"), ("foo scheduler", "baz"))),
+                "root": True,
             }
         }
         self.assertEquals(parse_schedulers(s), expected)
 
     def testTriggerableScheduler(self):
         s = [
-            Scheduler('base', ('upstream',)),
-            Triggerable('foo', ('bar',)),
+            Scheduler("base", ("upstream",)),
+            Triggerable("foo", ("bar",)),
         ]
         triggerables= {
-            'foo': ('upstream',),
+            "foo": ("upstream",),
         }
         expected = {
-            'base scheduler': {
-                'nodes': set(('base scheduler', 'upstream')),
-                'edges': set((('base scheduler', 'upstream'),)),
-                'root': True,
+            "base scheduler": {
+                "nodes": set(("base scheduler", "upstream")),
+                "edges": set((("base scheduler", "upstream"),)),
+                "root": True,
             },
-            'foo scheduler': {
-                'nodes': set(('foo scheduler', 'bar', 'upstream')),
-                'edges': set((('foo scheduler', 'bar'), ('upstream', 'foo scheduler'))),
-                'root': False,
+            "foo scheduler": {
+                "nodes": set(("foo scheduler", "bar", "upstream")),
+                "edges": set((("foo scheduler", "bar"), ("upstream", "foo scheduler"))),
+                "root": False,
             }
         }
         self.assertEquals(parse_schedulers(s, triggerables), expected)
 
     def testDependentScheduler(self):
         s = [
-            Scheduler('base', ('upstream',)),
-            Dependent('foo', ('bar',), upstream_name='base'),
+            Scheduler("base", ("upstream",)),
+            Dependent("foo", ("bar",), upstream_name="base"),
         ]
         expected = {
-            'base scheduler': {
-                'nodes': set(('base scheduler', 'upstream')),
-                'edges': set((('base scheduler', 'upstream'),)),
-                'root': True,
+            "base scheduler": {
+                "nodes": set(("base scheduler", "upstream")),
+                "edges": set((("base scheduler", "upstream"),)),
+                "root": True,
             },
-            'foo scheduler': {
-                'nodes': set(('foo scheduler', 'bar', 'upstream')),
-                'edges': set((('foo scheduler', 'bar'), ('upstream', 'foo scheduler'))),
-                'root': False,
+            "foo scheduler": {
+                "nodes": set(("foo scheduler", "bar", "upstream")),
+                "edges": set((("foo scheduler", "bar"), ("upstream", "foo scheduler"))),
+                "root": False,
             },
         }
         self.assertEquals(parse_schedulers(s), expected)
 
     def testDependentSchedulerMultipleUpstreamBuilders(self):
         s = [
-            Scheduler('base', ('upstream1', 'upstream2')),
-            Dependent('foo', ('bar',), upstream_name='base'),
+            Scheduler("base", ("upstream1", "upstream2")),
+            Dependent("foo", ("bar",), upstream_name="base"),
         ]
         expected = {
-            'base scheduler': {
-                'nodes': set(('base scheduler', 'upstream1', 'upstream2')),
-                'edges': set((('base scheduler', 'upstream1'), ('base scheduler', 'upstream2'))),
-                'root': True,
+            "base scheduler": {
+                "nodes": set(("base scheduler", "upstream1", "upstream2")),
+                "edges": set((("base scheduler", "upstream1"), ("base scheduler", "upstream2"))),
+                "root": True,
             },
-            'foo scheduler': {
-                'nodes': set(('foo scheduler', 'bar', 'upstream1', 'upstream2')),
-                'edges': set((('foo scheduler', 'bar'), ('upstream1', 'foo scheduler'), ('upstream2', 'foo scheduler'))),
-                'root': False,
+            "foo scheduler": {
+                "nodes": set(("foo scheduler", "bar", "upstream1", "upstream2")),
+                "edges": set((("foo scheduler", "bar"), ("upstream1", "foo scheduler"), ("upstream2", "foo scheduler"))),
+                "root": False,
             },
         }
         self.assertEquals(parse_schedulers(s), expected)
 
     def testAggregatingScheduler(self):
         s = [
-            Scheduler('base', ('upstream',)),
-            AggregatingScheduler('foo', ('bar',), upstreamBuilders=('upstream',)),
+            Scheduler("base", ("upstream",)),
+            AggregatingScheduler("foo", ("bar",), upstreamBuilders=("upstream",)),
         ]
         expected = {
-            'base scheduler': {
-                'nodes': set(('base scheduler', 'upstream')),
-                'edges': set((('base scheduler', 'upstream'),)),
-                'root': True,
+            "base scheduler": {
+                "nodes": set(("base scheduler", "upstream")),
+                "edges": set((("base scheduler", "upstream"),)),
+                "root": True,
             },
-            'foo scheduler': {
-                'nodes': set(('foo scheduler', 'bar', 'upstream')),
-                'edges': set((('foo scheduler', 'bar'), ('upstream', 'foo scheduler'))),
-                'root': False,
+            "foo scheduler": {
+                "nodes": set(("foo scheduler", "bar", "upstream")),
+                "edges": set((("foo scheduler", "bar"), ("upstream", "foo scheduler"))),
+                "root": False,
             },
         }
         self.assertEquals(parse_schedulers(s), expected)
 
     def testAggergatingSchedulerMultipleUpstreamBuilders(self):
         s = [
-            Scheduler('base', ('upstream1', 'upstream2')),
-            AggregatingScheduler('foo', ('bar',), upstreamBuilders=('upstream1', 'upstream2')),
+            Scheduler("base", ("upstream1", "upstream2")),
+            AggregatingScheduler("foo", ("bar",), upstreamBuilders=("upstream1", "upstream2")),
         ]
         expected = {
-            'base scheduler': {
-                'nodes': set(('base scheduler', 'upstream1', 'upstream2')),
-                'edges': set((('base scheduler', 'upstream1'), ('base scheduler', 'upstream2'))),
-                'root': True,
+            "base scheduler": {
+                "nodes": set(("base scheduler", "upstream1", "upstream2")),
+                "edges": set((("base scheduler", "upstream1"), ("base scheduler", "upstream2"))),
+                "root": True,
             },
-            'foo scheduler': {
-                'nodes': set(('foo scheduler', 'bar', 'upstream1', 'upstream2')),
-                'edges': set((('foo scheduler', 'bar'), ('upstream1', 'foo scheduler'), ('upstream2', 'foo scheduler'))),
-                'root': False,
+            "foo scheduler": {
+                "nodes": set(("foo scheduler", "bar", "upstream1", "upstream2")),
+                "edges": set((("foo scheduler", "bar"), ("upstream1", "foo scheduler"), ("upstream2", "foo scheduler"))),
+                "root": False,
             },
         }
         self.assertEquals(parse_schedulers(s), expected)
@@ -136,44 +137,44 @@ class TestMergeGraphInfo(unittest.TestCase):
     maxDiff = 2000
     def testSimpleMerge(self):
         graph_info = {
-            'base': {
-                'nodes': set(('base', 'upstream')),
-                'edges': set((('base', 'upstream'),)),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "upstream")),
+                "edges": set((("base", "upstream"),)),
+                "root": True,
             },
-            'foo': {
-                'nodes': set(('foo', 'bar', 'upstream')),
-                'edges': set((('foo', 'bar'), ('upstream', 'foo'))),
-                'root': False,
+            "foo": {
+                "nodes": set(("foo", "bar", "upstream")),
+                "edges": set((("foo", "bar"), ("upstream", "foo"))),
+                "root": False,
             },
         }
         expected = {
-            'base': {
-                'nodes': set(('base', 'upstream', 'foo', 'bar')),
-                'edges': set((('base', 'upstream'), ('upstream', 'foo'), ('foo', 'bar'))),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "upstream", "foo", "bar")),
+                "edges": set((("base", "upstream"), ("upstream", "foo"), ("foo", "bar"))),
+                "root": True,
             },
         }
         self.assertEquals(merge_graph_info(graph_info), expected)
 
     def testMultipleUpstreamMerge(self):
         graph_info = {
-            'base': {
-                'nodes': set(('base', 'upstream1', 'upstream2')),
-                'edges': set((('base', 'upstream1'), ('base', 'upstream2'))),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "upstream1", "upstream2")),
+                "edges": set((("base", "upstream1"), ("base", "upstream2"))),
+                "root": True,
             },
-            'foo': {
-                'nodes': set(('foo', 'bar', 'upstream1', 'upstream2')),
-                'edges': set((('foo', 'bar'), ('upstream1', 'foo'), ('upstream2', 'foo'))),
-                'root': False,
+            "foo": {
+                "nodes": set(("foo", "bar", "upstream1", "upstream2")),
+                "edges": set((("foo", "bar"), ("upstream1", "foo"), ("upstream2", "foo"))),
+                "root": False,
             },
         }
         expected = {
-            'base': {
-                'nodes': set(('base', 'upstream1', 'upstream2', 'foo', 'bar')),
-                'edges': set((('base', 'upstream1'), ('base', 'upstream2'), ('upstream1', 'foo'), ('upstream2', 'foo'), ('foo', 'bar'))),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "upstream1", "upstream2", "foo", "bar")),
+                "edges": set((("base", "upstream1"), ("base", "upstream2"), ("upstream1", "foo"), ("upstream2", "foo"), ("foo", "bar"))),
+                "root": True,
             },
         }
         self.assertEquals(merge_graph_info(graph_info), expected)
@@ -181,86 +182,106 @@ class TestMergeGraphInfo(unittest.TestCase):
     def testMergeWorksRegardlessOfOrder(self):
         from collections import OrderedDict
         graph_info = OrderedDict()
-        graph_info['foo'] = {
-            'nodes': set(('foo', 'bar', 'upstream1', 'upstream2')),
-            'edges': set((('foo', 'bar'), ('upstream1', 'foo'), ('upstream2', 'foo'))),
-            'root': False,
+        graph_info["foo"] = {
+            "nodes": set(("foo", "bar", "upstream1", "upstream2")),
+            "edges": set((("foo", "bar"), ("upstream1", "foo"), ("upstream2", "foo"))),
+            "root": False,
         }
-        graph_info['base'] = {
-            'nodes': set(('base', 'upstream1', 'upstream2')),
-            'edges': set((('base', 'upstream1'), ('base', 'upstream2'))),
-            'root': True,
+        graph_info["base"] = {
+            "nodes": set(("base", "upstream1", "upstream2")),
+            "edges": set((("base", "upstream1"), ("base", "upstream2"))),
+            "root": True,
         }
         expected = {
-            'base': {
-                'nodes': set(('base', 'upstream1', 'upstream2', 'foo', 'bar')),
-                'edges': set((('base', 'upstream1'), ('base', 'upstream2'), ('upstream1', 'foo'), ('upstream2', 'foo'), ('foo', 'bar'))),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "upstream1", "upstream2", "foo", "bar")),
+                "edges": set((("base", "upstream1"), ("base", "upstream2"), ("upstream1", "foo"), ("upstream2", "foo"), ("foo", "bar"))),
+                "root": True,
             },
         }
         self.assertEquals(merge_graph_info(graph_info), expected)
 
     def testMultilevelMerge(self):
         graph_info = {
-            'base': {
-                'nodes': set(('base', 'basebuilder')),
-                'edges': set((('base', 'basebuilder'),)),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "basebuilder")),
+                "edges": set((("base", "basebuilder"),)),
+                "root": True,
             },
-            'foo': {
-                'nodes': set(('foo', 'foobuilder', 'basebuilder')),
-                'edges': set((('foo', 'foobuilder'), ('basebuilder', 'foo'))),
-                'root': False,
+            "foo": {
+                "nodes": set(("foo", "foobuilder", "basebuilder")),
+                "edges": set((("foo", "foobuilder"), ("basebuilder", "foo"))),
+                "root": False,
             },
-            'bar': {
-                'nodes': set(('bar', 'barbuilder', 'foobuilder')),
-                'edges': set((('bar', 'barbuilder'), ('foobuilder', 'bar'))),
-                'root': False,
+            "bar": {
+                "nodes": set(("bar", "barbuilder", "foobuilder")),
+                "edges": set((("bar", "barbuilder"), ("foobuilder", "bar"))),
+                "root": False,
             }
         }
         expected = {
-            'base': {
-                'nodes': set(('base', 'basebuilder', 'foo', 'foobuilder',
-                              'bar', 'barbuilder')),
-                'edges': set((('base', 'basebuilder'), ('foo', 'foobuilder'), ('basebuilder', 'foo'),
-                              ('bar', 'barbuilder'),
-                              ('foobuilder', 'bar'))),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "basebuilder", "foo", "foobuilder",
+                              "bar", "barbuilder")),
+                "edges": set((("base", "basebuilder"), ("foo", "foobuilder"), ("basebuilder", "foo"),
+                              ("bar", "barbuilder"),
+                              ("foobuilder", "bar"))),
+                "root": True,
             }
         }
         self.assertEquals(merge_graph_info(graph_info), expected)
 
     def testDiamondMerge(self):
         graph_info = {
-            'base': {
-                'nodes': set(('base', 'basebuilder')),
-                'edges': set((('base', 'basebuilder'),)),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "basebuilder")),
+                "edges": set((("base", "basebuilder"),)),
+                "root": True,
             },
-            'foo': {
-                'nodes': set(('foo', 'foobuilder', 'basebuilder')),
-                'edges': set((('foo', 'foobuilder'), ('basebuilder', 'foo'))),
-                'root': False,
+            "foo": {
+                "nodes": set(("foo", "foobuilder", "basebuilder")),
+                "edges": set((("foo", "foobuilder"), ("basebuilder", "foo"))),
+                "root": False,
             },
-            'bar': {
-                'nodes': set(('bar', 'barbuilder', 'basebuilder')),
-                'edges': set((('bar', 'barbuilder'), ('basebuilder', 'bar'))),
-                'root': False,
+            "bar": {
+                "nodes": set(("bar", "barbuilder", "basebuilder")),
+                "edges": set((("bar", "barbuilder"), ("basebuilder", "bar"))),
+                "root": False,
             },
-            'merged': {
-                'nodes': set(('merged', 'mergedbuilder', 'foobuilder', 'barbuilder')),
-                'edges': set((('merged', 'mergedbuilder'), ('foobuilder', 'merged'), ('barbuilder', 'merged'))),
-                'root': False,
+            "merged": {
+                "nodes": set(("merged", "mergedbuilder", "foobuilder", "barbuilder")),
+                "edges": set((("merged", "mergedbuilder"), ("foobuilder", "merged"), ("barbuilder", "merged"))),
+                "root": False,
             }
         }
         expected = {
-            'base': {
-                'nodes': set(('base', 'basebuilder', 'foo', 'foobuilder',
-                              'bar', 'barbuilder', 'merged', 'mergedbuilder')),
-                'edges': set((('base', 'basebuilder'), ('foo', 'foobuilder'), ('basebuilder', 'foo'),
-                              ('bar', 'barbuilder'), ('basebuilder', 'bar'), ('merged', 'mergedbuilder'),
-                              ('foobuilder', 'merged'), ('barbuilder', 'merged'))),
-                'root': True,
+            "base": {
+                "nodes": set(("base", "basebuilder", "foo", "foobuilder",
+                              "bar", "barbuilder", "merged", "mergedbuilder")),
+                "edges": set((("base", "basebuilder"), ("foo", "foobuilder"), ("basebuilder", "foo"),
+                              ("bar", "barbuilder"), ("basebuilder", "bar"), ("merged", "mergedbuilder"),
+                              ("foobuilder", "merged"), ("barbuilder", "merged"))),
+                "root": True,
             }
         }
         self.assertEquals(merge_graph_info(graph_info), expected)
+
+
+class TestMergeNodes(unittest.TestCase):
+    def testMergeMultipleDownstream(self):
+        nodes = {"base", "foo 1/2", "foo 2/2"}
+        edges = {("base", "foo 1/2"), ("base", "foo 2/2")}
+        expected = {"base", "foo"}
+        self.assertEquals(merge_nodes(nodes, edges), expected)
+
+    def testMergeMultipleUpstream(self):
+        pass
+
+    def testUnmergeableUpstreams(self):
+        pass
+
+    def testUnmergeableDownstreams(self):
+        pass
+
+    def testCustomMergePattern(self):
+        pass
